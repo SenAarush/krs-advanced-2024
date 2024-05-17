2f783dc8b3d7b70c7fac12934244f42957e35201
SenAarush <senaarush@outlook.com>
2024-05-17 17:47:14 +0530

    completed login

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,13 +1,16 @@
 const UserModel = require('../models/user.model');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+
 const validators = require('../utils/validators');
+const helpers = require('../utils/helpers');
 
 const register = async (req, res) => {
     try {
         const { name, email, pass, cPass } = req.body;
-        const user = await UserModel.findOne({ email });
-        if (user) {
-            return res.send({ error: "user already exists" }).status(400);
+        if (!name || !email || !pass || !cPass) {
+            return res.send({ error: "unable to find required details" }).status(400);
         }
         if (pass !== cPass) {
             return res.send({ error: "password and confirm password do not match" }).status(400);
@@ -17,6 +20,10 @@ const register = async (req, res) => {
         }
         if (!validators.checkPass(pass)) {
             return res.send({ error: "invalid password" }).status(400);
+        }
+        const user = await UserModel.findOne({ email });
+        if (user) {
+            return res.send({ error: "user already exists" }).status(400);
         }
         const hashedPass = await bcrypt.hash(pass, 12);
         const newUser = new UserModel({ name, email, password: hashedPass });
@@ -29,7 +36,23 @@ const register = async (req, res) => {
 
 const login = async (req, res) => {
     try {
-        const { email, password, cPassword } = req.body;
+        const { email, pass, cPass } = req.body;
+        if (!email || !pass || !cPass) {
+            return res.send({ error: "unable to find required details" }).status(400);
+        }
+        if (pass !== cPass) {
+            return res.send({ error: "password and confirm password do not match" }).status(400);
+        }
+        const user = await UserModel.findOne({ email });
+        if (!user) {
+            return res.send({ error: "invalid credentials" }).status(400);
+        }
+        const isMatch = await bcrypt.compare(pass, user.password);
+        if (!isMatch) {
+            return res.send({ error: "invalid credentials" }).status(400);
+        }
+        const token = helpers.createJWT(user._id);
+        res.cookie('authToken', token, { httpOnly: true }).send({ message: "login successful" }).status(200);
     } catch (e) {
         res.json({ message: `error ${e.message}` }).status(400);
     }
